53ca7194f4fb696500ddf06211e0ce729552dfa8
Shashank Pandey <138484724+webvokess@users.noreply.github.com>
2025-07-19 23:21:24 +0530

    Create Lineseparator.tsx

---
+++ components/common/separator/Lineseparator.tsx
@@ -0,0 +1,9 @@
+import clsx from "clsx";
+import React from "react";
+
+export default function LineSeparator() {
+  return (
+    <div
+      className={clsx("w-full h-[1px] bg-grey-300")}></div>
+  );
+}
